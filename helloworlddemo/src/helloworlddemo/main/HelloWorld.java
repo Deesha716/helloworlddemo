39063378ea1ff28ca9b8408bee4a9b8fc833de62
Deesha716 <deeshasrawat@gmail.com>
2022-02-03 17:54:33 +0530

    My first hello world project

---
+++ helloworlddemo/src/helloworlddemo/main/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package helloworlddemo.main;
+
+/**
+ * @author admin
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello everyone welcome to eclipse");
+	}
+
+}
